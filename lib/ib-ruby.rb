9d3af991f94a15cf77db793739501ca98fb61607
Roberto Vasquez Angel <robotex@robotex.de>
2012-05-08 14:43:57 +0200

    Using initializers to do lazy requires

--- lib/ib-ruby.rb
+++ lib/ib-ruby.rb
@@ -1,3 +1,5 @@
+require 'ib-ruby/ib'
+
 if defined?(Rails) && Rails.respond_to?('env')
   require 'ib-ruby/db'
 end
@@ -7,25 +9,25 @@ module IB
   # By default there is no DB backend, unless specifically requested
   # require 'ib-ruby/db' # to make all IB models database-backed
   DB ||= false
-
-  require 'ib-ruby/version'
-  require 'ib-ruby/extensions'
-  require 'ib-ruby/errors'
-  require 'ib-ruby/constants'
-  require 'ib-ruby/connection'
-
-  require 'ib-ruby/models'
-  Datatypes = Models # Flatten namespace (IB::Contract instead of IB::Models::Contract)
-  include Models # Legacy alias
-
-  require 'ib-ruby/messages'
-  IncomingMessages = Messages::Incoming # Legacy alias
-  OutgoingMessages = Messages::Outgoing # Legacy alias
-
-  require 'ib-ruby/symbols'
   
   if defined?(Rails) && Rails.respond_to?('env')
     require 'ib-ruby/engine'
+  else
+    require 'ib-ruby/version'
+    require 'ib-ruby/extensions'
+    require 'ib-ruby/errors'
+    require 'ib-ruby/constants'
+    require 'ib-ruby/connection'
+
+    require 'ib-ruby/models'
+    Datatypes = Models # Flatten namespace (IB::Contract instead of IB::Models::Contract)
+    include Models # Legacy alias
+
+    require 'ib-ruby/messages'
+    IncomingMessages = Messages::Incoming # Legacy alias
+    OutgoingMessages = Messages::Outgoing # Legacy alias
+
+    require 'ib-ruby/symbols'  
   end
 end
 IbRuby = IB
--- lib/ib-ruby/engine.rb
+++ lib/ib-ruby/engine.rb
@@ -2,5 +2,27 @@ module IB
   class Engine < Rails::Engine
     config.to_prepare do
     end  
+    
+    initializer "ib-ruby.active_record" do |app|
+      ActiveSupport.on_load :active_record do
+        module ::IB
+          require 'ib-ruby/version'
+          require 'ib-ruby/extensions'
+          require 'ib-ruby/errors'
+          require 'ib-ruby/constants'
+          require 'ib-ruby/connection'
+
+          require 'ib-ruby/models'
+          Datatypes = Models # Flatten namespace (IB::Contract instead of IB::Models::Contract)
+          include Models # Legacy alias
+
+          require 'ib-ruby/messages'
+          IncomingMessages = Messages::Incoming # Legacy alias
+          OutgoingMessages = Messages::Outgoing # Legacy alias
+
+          require 'ib-ruby/symbols'  
+        end
+      end
+    end
   end
 end
