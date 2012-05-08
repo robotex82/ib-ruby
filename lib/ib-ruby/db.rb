d14e19f325ec41908f6576bd52ceca36b1293ba0
Roberto Vasquez Angel <robotex@robotex.de>
2012-05-08 12:33:47 +0200

    Removed forced connection in IB::DB

--- lib/ib-ruby/db.rb
+++ lib/ib-ruby/db.rb
@@ -12,7 +12,7 @@ module IB
     # Establish DB connection and do other plumbing here
     def self.connect config
       #log.warn "Starting Database connection"
-      ActiveRecord::Base.establish_connection(config)
+      # ActiveRecord::Base.establish_connection(config)
       #ActiveRecord.colorize_logging = false
 
       # Get rid of nasty conversion issues
