cd7394a83867278e9008f68192b4a98ec744da2e
Roberto Vasquez Angel <robotex@robotex.de>
2012-05-08 15:31:36 +0200

    Models are not required per se. Should autoload with rails or be required manually

--- lib/ib-ruby/models.rb
+++ lib/ib-ruby/models.rb
@@ -4,12 +4,12 @@ module IB
     require 'ib-ruby/models/model_properties'
     require 'ib-ruby/models/model'
 
-    require 'ib-ruby/models/contract'
-    require 'ib-ruby/models/order_state'
-    require 'ib-ruby/models/order'
-    require 'ib-ruby/models/combo_leg'
-    require 'ib-ruby/models/execution'
-    require 'ib-ruby/models/bar'
+#    require 'ib-ruby/models/contract'
+#    require 'ib-ruby/models/order_state'
+#    require 'ib-ruby/models/order'
+#    require 'ib-ruby/models/combo_leg'
+#    require 'ib-ruby/models/execution'
+#    require 'ib-ruby/models/bar'
   end
 end
 
