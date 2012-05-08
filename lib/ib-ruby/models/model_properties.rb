f09916d2edba3752d597690ef4b28ad17b5d9778
Roberto Vasquez Angel <robotex@robotex.de>
2012-05-08 13:18:29 +0200

    Attempt to fix connection code in ModelProperties.initialize

--- lib/ib-ruby/models/model_properties.rb
+++ lib/ib-ruby/models/model_properties.rb
@@ -133,7 +133,7 @@ module IB
         # Extending AR-backed Model class with attribute defaults
         if defined?(ActiveRecord::Base) && ancestors.include?(ActiveRecord::Base)
           def initialize attributes={}, opts={}
-            ActiveRecord::Base.establish_connection(ActiveRecord::Base.connection_config) unless ActiveRecord::Base.connected?
+            ActiveRecord::Base.establish_connection(Rails::Application::Configuration.database_configuration()) unless ActiveRecord::Base.connected?
             super default_attributes.merge(attributes), opts
           end
         else
